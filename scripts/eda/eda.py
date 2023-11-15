8873c84adca056425bea3371c47af108a0a76b9c
Shorzinator <shorz2905@gmail.com>
2023-11-14 21:13:56 -0600

    Increasing modularity of EDA script and updating output graphs

--- scripts/eda/eda.py
+++ scripts/eda/eda.py
@@ -4,112 +4,48 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Set Pandas display option
 pd.set_option('display.max_columns', None)
 
+# Define paths to data
 business_path = "C:\\Users\\shour\\PycharmProjects\\MangiaMetrics_628\\data\\raw\\business.json"
-# business_df = pd.read_json(business_path, lines=True, chunksize=10000)
 
-# Define chunk size
-chunk_size = 10000  # Adjust this based on your machine's memory capability
-
-# Initialize an empty DataFrame to hold filtered data
+# Initialize DataFrame to hold business data
 business_data_pa = pd.DataFrame()
 
 
-# Function to process and filter each chunk
-def process_business_chunk(chunk):
-    # Filter for businesses in Pennsylvania (PA)
-    return chunk[chunk['state'] == 'PA']
+# Function definitions
+def load_data_in_chunks(file_path, chunk_size, filter_state='PA'):
+    temp_df = pd.DataFrame()
+    for chunk in pd.read_json(file_path, lines=True, chunksize=chunk_size, dtype={'postal_code': str}):
+        temp_df = pd.concat([temp_df, chunk[chunk['state'] == filter_state]])
+    return temp_df
 
 
-# Read and process the file in chunks
-for chunk in pd.read_json(business_path, lines=True, chunksize=chunk_size, dtype={'postal_code': str}):
-    filtered_chunk = process_business_chunk(chunk)
-    business_data_pa = pd.concat([business_data_pa, filtered_chunk])
-
-
-# Display the first few rows of the filtered DataFrame
-# print(business_data_pa.head())
-
-# Part 2
 def count_categories(dataframe):
     category_counts = Counter()
     for row in dataframe['categories']:
-        if row:  # Check if the category is not NaN
+        if row:
             categories = row.split(', ')
             category_counts.update(categories)
     return category_counts
 
 
-# Count categories in the PA business data
-pa_category_counts = count_categories(business_data_pa)
-
-
-# Display the most common categories
-# print("Most common categoris in PA:")
-# for category, count in pa_category_counts.most_common(10):
-#     print(f"{category}: {count}")
-
-# Plotting the top categories
-# top_categories = pa_category_counts.most_common(10)
-# categories, counts = zip(*top_categories)
-# plt.figure(figsize=(10, 6))
-# plt.bar(categories, counts)
-# plt.xticks(rotation=45)
-# plt.title('Top 10 business categories in PA')
-# plt.xlabel('Category')
-# plt.ylabel('Count')
-
-
-# plt.savefig('Top_10_businesses_in_PA.png')
-# plt.show()
-
-# Part 3
-# Adjusted function to handle None values in 'categories'
 def extract_restaurant_types(row):
     if row['categories'] and 'Restaurants' in row['categories']:
-        # Extract and return the list of restaurant types/categories
         return [category.strip() for category in row['categories'].split(',') if category.strip() != 'Restaurants']
     return []
 
 
-# Apply the function to the DataFrame
-business_data_pa['restaurant_types'] = business_data_pa.apply(extract_restaurant_types, axis=1)
-
-# Flatten the list of types and count occurrences
-restaurant_type_counts = pd.Series(
-    [item for sublist in business_data_pa['restaurant_types'] for item in sublist]).value_counts()
-
-# Display the most common restaurant types
-# print("Most common restaurant types in PA:")
-# print(restaurant_type_counts.head(10))
-
-# Plot the results
-restaurant_type_counts.head(10).plot(kind='bar', figsize=(10, 6), title='Top 10 Restaurant Types in PA')
-plt.xlabel('Restaurant Type')
-plt.ylabel('Count')
-# plt.savefig('Top_10_restaurant_types_in_PA.pnga')
-# plt.show()
-
-
-# Part 4.1
-
-# Define function to filter specific restaurant categories
 def filter_specific_categories(dataframe, category_list):
-    filtered_df = dataframe[
+    return dataframe[
         dataframe['restaurant_types'].apply(lambda types: any(cat in types for cat in category_list))]
-    return filtered_df
 
 
-# Filter for 'Italian' and 'Breakfast & Brunch' categories
-italian_restaurants = filter_specific_categories(business_data_pa, ['Italian'])
-breakfast_brunch_restaurants = filter_specific_categories(business_data_pa, ['Breakfast & Brunch'])
-
-
-# Function to calculate basic metrics for a category
 def calculate_basic_metrics(df, category_name):
     average_rating = df['stars'].mean()
     total_restaurants = len(df)
@@ -117,9 +53,43 @@ def calculate_basic_metrics(df, category_name):
     print(f"Metrics for {category_name}:")
     print(f"Total Restaurants: {total_restaurants}")
     print(f"Average Rating: {average_rating:.2f}")
-    print(f"Average Review Count: {average_review_count:.2f}\n")
+    print(f"Average Review Count: {average_review_count:.2f}")
 
 
-# Calculate and display metrics for Italian and Breakfast & Brunch restaurants
-calculate_basic_metrics(italian_restaurants, 'Italian')
-calculate_basic_metrics(breakfast_brunch_restaurants, 'Breakfast & Brunch')
+def plot_category_counts(counter, title, file_name):
+    # Convert the Counter object to a Panda Series
+    series = pd.Series(counter)
+    top_categories = series.nlargest(10)
+    top_categories.plot(kind='bar', figsize=(10, 6))
+    plt.xticks(rotation=45)
+    plt.title(title)
+    plt.xlabel('Category')
+    plt.ylabel('Count')
+    plt.tight_layout()  # Adjust layout to fit labels
+    plt.savefig(file_name)
+    plt.close()
+
+
+# Main function to call all tasks
+def main():
+    chunk_size = 10000  # Adjust based on your system's memory capability
+    business_data_pa = load_data_in_chunks(business_path, chunk_size)
+
+    # Uncomment the following lines to perform the respective tasks
+    pa_category_counts = count_categories(business_data_pa)
+    plot_category_counts(pa_category_counts, 'Top 10 Business Categories in PA', 'Top_10_business_categories_in_PA.png')
+
+    business_data_pa['restaurant_types'] = business_data_pa.apply(extract_restaurant_types, axis=1)
+    restaurant_type_counts = pd.Series(
+        [item for sublist in business_data_pa['restaurant_types'] for item in sublist]).value_counts()
+    plot_category_counts(restaurant_type_counts, 'Top 10 Restaurant Types in PA', 'Top_10_restaurant_types_in_PA.png')
+
+    italian_restaurants = filter_specific_categories(business_data_pa, ['Italian'])
+    breakfast_brunch_restaurants = filter_specific_categories(business_data_pa, ['Breakfast & Brunch'])
+    calculate_basic_metrics(italian_restaurants, 'Italian')
+    calculate_basic_metrics(breakfast_brunch_restaurants, 'Breakfast & Brunch')
+
+
+# Call the main function
+if __name__ == "__main__":
+    main()

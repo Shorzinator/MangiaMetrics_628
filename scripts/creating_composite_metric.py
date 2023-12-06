1a11ef5021eab8871b3533c3e7dc044771461300
Shorzinator <shorz2905@gmail.com>
2023-12-05 20:47:11 -0600

    Updating composite metric script

--- scripts/creating_composite_metric.py
+++ scripts/creating_composite_metric.py
@@ -1,11 +1,16 @@
+import os
+
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
+from scripts.utility.path_utils import get_path_from_root
+
 # Load the datasets
-final_dp03 = pd.read_csv('C:\\Users\\shour\\PycharmProjects\\MangiaMetrics_628\\data\\final\\final_dp03.csv')
-final_dp04 = pd.read_csv('C:\\Users\\shour\\PycharmProjects\\MangiaMetrics_628\\data\\final\\final_dp04.csv')
-final_dp05 = pd.read_csv('C:\\Users\\shour\\PycharmProjects\\MangiaMetrics_628\\data\\final\\final_dp05.csv')
-top_features = pd.read_csv('C:\\Users\\shour\\PycharmProjects\\MangiaMetrics_628\\results\\modeling\\pca\\top_features_merged.csv')
+common_path = get_path_from_root("data", "final")
+final_dp03 = pd.read_csv(os.path.join(common_path, "final_dp03.csv"))
+final_dp04 = pd.read_csv(os.path.join(common_path, "final_dp04.csv"))
+final_dp05 = pd.read_csv(os.path.join(common_path, "final_dp05.csv"))
+top_features = pd.read_csv(get_path_from_root("results", "modeling", "pca", "top_features_merged.csv"))
 
 # Merging the datasets on 'ZIP_CODE'
 merged_data = pd.merge(final_dp03, final_dp04, on='ZIP_CODE', how='outer')
@@ -42,5 +47,6 @@ composite_metric_df = pd.DataFrame({
 composite_metric_df['Composite_Metric'] = composite_metric_df['Composite_Metric'] * 100
 
 # Save the final dataframe to a CSV file
-composite_metric_df.to_csv('C:\\Users\\shour\\PycharmProjects\\MangiaMetrics_628\\data\\interim\\composite_metric.csv',
+path = get_path_from_root("data", "interim")
+composite_metric_df.to_csv(os.path.join(path, "composite_metric.csv"),
                            index=False)
